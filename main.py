cbcbd19794bdafc1b33b75789ef7969e83d08fc3
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-29 16:44:30 +0530

    initialised the project

---
+++ main.py
@@ -0,0 +1,10 @@
+import transformers
+print(transformers.__version__)
+
+### Importing the Libraries ###
+import librosa
+import torch
+import IPython.display as display
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+import numpy as np
+
